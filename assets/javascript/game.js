5f1af9f0fea93ca468339dc9e4d74b6bc30875c0
kbub27 <kalebmerriman27@gmail.com>
2019-04-05 20:32:51 -0700

    added some firebase and some functions to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,4 +11,48 @@ $(document).ready(function() {
     firebase.initializeApp(config);
 
     var database = firebase.database();
+    var player = {
+        user1: {
+            assigned: false
+        },
+        user2: {
+            assigned: false
+        }
+    };
+    var inGame = false;
+
+    var connectionsRef = database.ref('/connections');
+
+    var connectedRef = database.ref('.info/connected');
+
+    connectedRef.on("value", function(snap) {
+
+        // If they are connected..
+        if (snap.val()) {
+      
+          // Add user to the connections list.
+          var connection = connectionsRef.push(true);
+      
+          // Remove user from the connection list when they disconnect.
+          connection.onDisconnect().remove();
+        }
+    });
+
+    connectionsRef.on('value', function (snapshot) {
+            $('#veiwers').text('You have ' + snapshot.numChildren() + ' veiwers watching you play now!');
+    });
+
+    function joinGame() {
+        if (player.user1.assigned && player.user2.assigned) {
+            alert('Already two users playing the Game')
+        } else if (player.user1.assigned) {
+            player.user2.assigned = true;
+        } else {
+            player.user1.assigned = true;
+        }
+    };
+
+    function leaveGame() {
+        
+    }
 })
